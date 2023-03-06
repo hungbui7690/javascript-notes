14f5414a710a2d5558971c1f5b73f38086d702f1
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:29:49 -0700

    FormData API P5: Iteration

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 /*
-  FormData API P4: keys, values, entries
+  FormData API P5: Iteration
 */
 
 const form = document.querySelector('.form')
-
-// (***) must select all the inputs
-const nameInput = document.querySelector('.name-input')
-const emailInput = document.querySelector('.email-input')
-const passwordInput = document.querySelector('.password-input')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const formData = new FormData(e.currentTarget)
 
-  const keys = [...formData.keys()]
-  console.log(keys)
-
-  const values = [...formData.values()]
-  console.log(values)
-
-  // (***) return array of arrays > [[key, value],[],[]]
-  const entries = [...formData.entries()]
-  console.log(entries)
+  // (***)
+  for (let [name, value] of formData) {
+    console.log(`${name} = ${value}`)
+  }
 })
