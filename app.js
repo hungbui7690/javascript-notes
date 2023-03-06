da5efac176249634dfa4835baa3f08fa3cb0b914
Hung Bui <hungbui7690@gmail.com>
2023-03-06 16:26:10 -0700

    Dynamic Object Keys P4: Dynamically Modify Object

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 /*
-  Dynamic Object Keys P3: Variables
-
+  Dynamic Object Keys P4: modify object
 */
 
-// (1)
-let appState = 'loading'
-appState = 'error'
-
-// (2)
-const app = {
-  [appState]: true,
+const state = {
+  loading: true,
+  name: '',
+  job: '',
 }
-console.log(app) // {error: true}
 
-// (3)
-const keyName = 'computer'
-app[keyName] = 'apple'
-console.log(app) // {error: true, computer: 'apple'}
+// (***)
+function modifyState(key, value) {
+  state[key] = value
+}
+
+// (1) update
+modifyState('name', 'Joe')
+console.log(state) // {loading: true, name: 'Joe', job: ''}
+
+// (2) create property
+modifyState('products', ['computer', 'laptop'])
+console.log(state) // {loading: true, name: 'Joe', job: '', products: Array(2)}
