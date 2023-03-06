d108b33ceea1b9d72b9fa46870f87754283e782f
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:43:24 -0700

    FormData API P8: Reset Form to Default

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /*
-  FormData API P7: Convert FormData to Object
+  FormData API P8: Reset Form to Default
+  - try to change the value of inputs > then click submit
 */
 
 const form = document.querySelector('.form')
@@ -9,10 +10,13 @@ form.addEventListener('submit', (e) => {
 
   const formData = new FormData(e.currentTarget)
 
-  // (***)
+  // (1)
   formData.append('test', 1234)
 
-  // (***) convert to object > we can use this to send data to server using Axios or fetch()
+  // (2)
   const formObject = Object.fromEntries(formData)
   console.log(formObject)
+
+  // (3) reset to default
+  e.currentTarget.reset()
 })
