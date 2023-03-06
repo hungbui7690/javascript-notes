adf112b0577880b2ff74f59ffb46ace2cb829e4f
Hung Bui <hungbui7690@gmail.com>
2023-03-06 16:03:06 -0700

    Async JS: Replace Callbacks with Promise

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
 /*
-  Async JS: Promise
-  - new Promise((resolve, reject) => {})
-    + resolve()
-    + reject()
+  Async JS: Replace Callbacks with Promise
 
-  - Consume Promise: 
-    + .then()
-    + .catch()
+  (1) return new Promise()
+  (4) we can see that, it does not work here > to consume promise, we need to use .then() & .catch > cannot use callback
 
 */
 
-// (1) create promise (async function)
-const p = new Promise((resolve, reject) => {
-  let err = false
+console.log('Before')
 
-  // kick out some async work ...
-  setTimeout(() => {
-    if (!err) {
-      resolve({ user: 'Joe Doe' }) // pending => resolved/fulfilled
-    } else reject(new Error('Error Message')) // pending => rejected
-  }, 2000)
+// (4) we can see that, it does not work here > to consume promise, we need to use .then() & .catch
+getUser(1, (user) => {
+  getRepositories(user, (repo) => {
+    getCommits(repo, (commit) => {
+      console.log(commit)
+    })
+  })
 })
+console.log('After')
 
-// (2) consume promise
-p.then((result) => console.log(result)).catch((err) => console.log(err))
+// (1)
+function getUser(id) {
+  // [a] return Promise + remove callback param
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a User from database...')
+
+      // [b] change to resolve()
+      resolve({ id, githubUser: 'Joe' })
+    }, 2000)
+  })
+}
+
+// (2) do similar to getUser
+function getRepositories(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting Repositories...')
+      resolve(['repo1', 'repo2', 'repo3'])
+    }, 2000)
+  })
+}
+
+// (3) do similar to getUser
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting Commits...')
+      resolve(['commit 1', 'commit 2', 'commit 3', 'commit 4'])
+    }, 2000)
+  })
+}
---
+++ promise.js
@@ -0,0 +1,26 @@
+/*
+  Async JS: Promise
+  - new Promise((resolve, reject) => {})
+    + resolve()
+    + reject()
+
+  - Consume Promise: 
+    + .then()
+    + .catch()
+
+*/
+
+// (1) create promise (async function)
+const p = new Promise((resolve, reject) => {
+  let err = false
+
+  // kick out some async work ...
+  setTimeout(() => {
+    if (!err) {
+      resolve({ user: 'Joe Doe' }) // pending => resolved/fulfilled
+    } else reject(new Error('Error Message')) // pending => rejected
+  }, 2000)
+})
+
+// (2) consume promise
+p.then((result) => console.log(result)).catch((err) => console.log(err))
