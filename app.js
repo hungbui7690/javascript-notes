0a43967875a5c65ef967bd5a99430064c5d5993d
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:34:06 -0700

    FormData API P6: Common Methods

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  FormData API P5: Iteration
+  FormData API P5: Common Methods
 */
 
 const form = document.querySelector('.form')
@@ -9,8 +9,22 @@ form.addEventListener('submit', (e) => {
 
   const formData = new FormData(e.currentTarget)
 
-  // (***)
-  for (let [name, value] of formData) {
-    console.log(`${name} = ${value}`)
-  }
+  // (1) get by "name attribute"
+  const name = formData.get('name')
+  console.log(name)
+
+  const x = formData.get('x')
+  console.log(x) // returns null since "x attribute" does not exist
+
+  // (2) add()
+  formData.append('age', 26)
+  console.log(formData.get('age'))
+
+  // (3) delete()
+  formData.delete('age')
+  console.log([...formData.keys()])
+
+  // (4) has()
+  console.log(formData.has('name'))
+  console.log(formData.has('x'))
 })
