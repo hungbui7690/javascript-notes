c2ca2f5c7bb7d2ca1ea9f7a2e0ccfd345d1ac6a7
Hung Bui <hungbui7690@gmail.com>
2023-03-06 16:35:30 -0700

    Fetch API P1: Initial Setup

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+  Fetch API P1: Initial Setup
+
+*/
+
+const getTextBtn = document.getElementById('getText')
+const getUsersBtn = document.getElementById('getUsers')
+const getPostsBtn = document.getElementById('getPosts')
+const addPostBtn = document.getElementById('addPost')
