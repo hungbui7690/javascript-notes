c4d17ada70ac658eea2b18aa3e41d2e1d4fa2b06
Hung Bui <hungbui7690@gmail.com>
2023-03-06 16:42:45 -0700

    Fetch API P3: Fetch JSON Data

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  Fetch API P2: Fetch Text File
+  Fetch API P3: Fetch JSON Data
 
 */
 
@@ -8,7 +8,6 @@ const getUsersBtn = document.getElementById('getUsers')
 const getPostsBtn = document.getElementById('getPosts')
 const addPostBtn = document.getElementById('addPost')
 
-// (1)
 function getText() {
   fetch('sample.txt')
     .then((res) => res.text())
@@ -18,5 +17,26 @@ function getText() {
     .catch((err) => console.log(err))
 }
 
-// (2)
 getTextBtn.addEventListener('click', getText)
+
+// (1)
+function getUsers() {
+  fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Users</h2>'
+      data.forEach(function (user) {
+        output += `
+          <ul class="list-group mb-3">
+            <li class="list-group-item">ID: ${user.id}</li>
+            <li class="list-group-item">Name: ${user.name}</li>
+            <li class="list-group-item">Email: ${user.email}</li>
+          </ul>
+      `
+      })
+      document.getElementById('output').innerHTML = output
+    })
+}
+
+// (2)
+getUsersBtn.addEventListener('click', getUsers)
