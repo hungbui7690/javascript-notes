9b0c44879f6de815adbf9f3e0533c7e50ef7e2b6
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:53:42 -0700

    Async JS: Callback P1

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 /*
-  Async JS: Async Patterns
-
-  - with async function, we can not consume it like normal
-  - there are 3 ways to consume an async function:
-    - callback
-    - promise
-    - async/await
+  Async JS: Callback P1
 
 */
 
 console.log('Before')
 
-const user = getUser(1)
-console.log(user) // (***) undefined
-
+// 3. use callback here
+getUser(1, (user) => {
+  console.log(user)
+  // Ex: after having user, we want to get all the repos of that user
+})
 console.log('After')
 
-function getUser(id) {
+// 1. Add param "callback"
+// 2. Change return to callback()
+function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a User from database...')
-    return { id, githubUser: 'Joe' }
+    callback({ id, githubUser: 'Joe' })
   })
 }
