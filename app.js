f0b1217651818f69aaf16202fa0f16730e355961
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:49:32 -0700

    Async JS: Intro

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 /*
-  FormData API P8: Reset Form to Default
-  - try to change the value of inputs > then click submit
+  Async JS: Intro
 */
 
-const form = document.querySelector('.form')
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const formData = new FormData(e.currentTarget)
-
-  // (1)
-  formData.append('test', 1234)
-
-  // (2)
-  const formObject = Object.fromEntries(formData)
-  console.log(formObject)
-
-  // (3) reset to default
-  e.currentTarget.reset()
+console.log('Before')
+setTimeout(() => {
+  console.log('Reading a User from database...')
 })
+console.log('After')
