7355cc21a5397344401c804264eb7d63770e842b
Hung Bui <hungbui7690@gmail.com>
2023-03-06 16:44:23 -0700

    Fetch API P4: Get Data from External API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  Fetch API P3: Fetch JSON Data
+  Fetch API P4: Get Data from External API
 
 */
 
@@ -19,7 +19,6 @@ function getText() {
 
 getTextBtn.addEventListener('click', getText)
 
-// (1)
 function getUsers() {
   fetch('users.json')
     .then((res) => res.json())
@@ -38,5 +37,25 @@ function getUsers() {
     })
 }
 
-// (2)
 getUsersBtn.addEventListener('click', getUsers)
+
+// (1)
+function getPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Posts</h2>'
+      data.forEach(function (post) {
+        output += `
+        <div class="card card-body mb-3">
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+        </div>
+      `
+      })
+      document.getElementById('output').innerHTML = output
+    })
+}
+
+// (2)
+getPostsBtn.addEventListener('click', getPosts)
