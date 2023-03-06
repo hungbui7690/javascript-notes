8a35adc70719c28e4e38cda95dfec77de07ab5f5
Hung Bui <hungbui7690@gmail.com>
2023-03-06 15:24:18 -0700

    FormData API P2: Why

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 /*
-  FormData API P1: Setup
+  FormData API P2: Why
 */
 
 const form = document.querySelector('.form')
 
+// (***) must select all the inputs
+const nameInput = document.querySelector('.name-input')
+const emailInput = document.querySelector('.email-input')
+const passwordInput = document.querySelector('.password-input')
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+
+  // (***) to get the values, we must go through many steps
+  const nameValue = nameInput.value
+  const emailValue = emailInput.value
+  const passwordValue = passwordInput.value
+
+  console.log(nameValue, emailValue, passwordValue)
+
+  // (***)
+  nameInput.value = ''
+  emailInput.value = ''
+  passwordInput.value = ''
 })
