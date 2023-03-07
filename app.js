69bdef6ab535199175e1b5451957b61dd7294965
Hung Bui <hungbui7690@gmail.com>
2023-03-06 19:46:03 -0700

    Axios P14: Intercepting Requests & Responses

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 /*
-  Axios P13: Cancellation 2
-  
-  - Setting the timeout property in an axios call handles response related timeouts.
-
-  - In some cases (e.g. network connection becomes unavailable) an axios call would benefit from cancelling the connection early. Without cancellation, the axios call can hang until the parent code/stack times out (might be a few minutes in a server-side applications).
-
-  - To terminate an axios call you can use following methods:
-      + signal
-      + cancelToken (deprecated)
-  
-  - Combining timeout and cancellation method (e.g. signal) should cover response related timeouts AND connection related timeouts.
+  Axios P14: Intercepting Requests & Responses
+  - You can intercept requests or responses before they are handled by then or catch.
 
 */
 
-const controller = new AbortController()
-
-function cancellation() {
+function getTodos() {
   axios
-    .get('/foo/bar', {
-      signal: AbortSignal.timeout(5000), //Aborts request after 5 seconds
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+      timeout: 5000,
     })
-    .then(function (response) {
-      //...
-    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err))
 }
 
-document.getElementById('cancel').addEventListener('click', cancellation)
+// (***) intercept request
+axios.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    )
+
+    return config
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// (***) intercept response
+axios.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log(response)
+    response.data[0].userId = 999 // modify response data
+    return response
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
+
+document.getElementById('get').addEventListener('click', getTodos)
--- methods.js
+++ methods.js
@@ -1,25 +1,4 @@
-// GET Method
-function getTodos() {
-  axios
-    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
-      timeout: 5000,
-    })
-    .then((res) => showOutput(res))
-    .catch((err) => console.error(err))
-}
 document.getElementById('get').addEventListener('click', getTodos)
-
-// POST REQUEST
-function addTodo() {
-  axios
-    .post('https://jsonplaceholder.typicode.com/todos', {
-      title: 'New Todo',
-      completed: false,
-    })
-    .then((res) => showOutput(res))
-    .catch((err) => console.error(err))
-}
-document.getElementById('post').addEventListener('click', addTodo)
 
 // PUT/PATCH REQUEST: must have id + body
 function updateTodo() {
@@ -103,3 +82,50 @@ function errorHandling() {
     })
 }
 document.getElementById('error').addEventListener('click', errorHandling)
+
+// CANCELLATION
+const controller = new AbortController()
+function cancellation() {
+  axios
+    .get('/foo/bar', {
+      signal: AbortSignal.timeout(5000), //Aborts request after 5 seconds
+    })
+    .then(function (response) {
+      //...
+    })
+}
+document.getElementById('cancel').addEventListener('click', cancellation)
+
+// INTERCEPTIONS
+axios.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    )
+
+    return config
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// (***) intercept response
+axios.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log(response)
+    response.data[0].userId = 999 // modify response data
+    return response
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
