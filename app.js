ede1768efc1ce97d4bdb8ada5111243b414d9c3e
Hung Bui <hungbui7690@gmail.com>
2023-03-07 02:29:52 -0700

    Intersection Observer P3

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 /*
-  Intersection Observer P2
-  - entries: all the things that have been intersected or un-intersected  
+  Intersection Observer P3
+  - remove "show" for the first card in html
 
-  
-  - some properties:
-    + intersectionRatio: 1 > that element is showed full on the screen
-    + isIntersecting: is is visible to user
-    + boundingClientRect: actual shape of the element
-    + intersectionRect: how much that element is on the screen > first card is 100% on the screen > intersectionRect === boundingClientRect
-    + rootBounds: screen
-    + target: that element
+  (***) as soon as 1px of the element is visible, add 'show' class
 
-
+  (***) test > scroll fast to see
 */
 
-// (1) select all the cards
 const cards = document.querySelectorAll('.card')
 
-// (2) create observer
 const observer = new IntersectionObserver((entries) => {
-  console.log(entries)
+  // (1)
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show', entry.isIntersecting) // toggle based on condition: entry.isIntersecting
+  })
 })
 
-// (3) (***) must be element type > cards === NodeList > cards[0] === Element
-observer.observe(cards[0]) // intersectionRatio: 1
-observer.observe(cards[1]) // intersectionRatio: 1 > though invisible, but still on the screen
+// (2)
+cards.forEach((card) => {
+  observer.observe(card)
+})
