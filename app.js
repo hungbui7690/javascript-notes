4d45a983db7b800d7d4f43073290338a1d93d386
Hung Bui <hungbui7690@gmail.com>
2023-03-07 01:58:15 -0700

    Debounce P3

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 /*
-  Debounce P2:
-  - technique to improve performance of certain functions that are repeatedly called
-  - setup threshold time for a function > determines how long to wait before that exec that function again
-    > example: user types in the search box > we can use debounce to wait until user pauses typing b4 executing the search function > prevent unnecessary calling the function multiple times, while user is still typing
-
-
-  - logic: we want to run the callback fn only 2s after the last click 
+  Debounce P3:
+  - with the setup below, we cannot log 'click ✈' 
+  - note that the id keeps increasing after each click 
+  > reason because the timeout is clear right away, after we click
 */
 
 const btn = document.querySelector('.btn')
 
-// (1) use setTimeout()
 const debounce = () => {
-  setTimeout(() => {
-    console.log('clicked')
+  // (1) save to variable
+  const timeoutID = setTimeout(() => {
+    console.log('clicked ✈')
   }, 2000)
+
+  console.log(timeoutID)
+
+  // (2) clear
+  clearTimeout(timeoutID)
+  console.log('hello')
 }
 
-// (2) pass ref > now, it's not correct, since it shows multiple console log after 2s > we just want to have 1 log
 btn.addEventListener('click', debounce)
