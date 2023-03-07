28048dba0c7bc6e69b0a3e63d1371d7bdd0cc0fa
Hung Bui <hungbui7690@gmail.com>
2023-03-06 19:25:03 -0700

    Axios P10: Error Handling 1

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 /*
-  Axios P9: Transforming Request & Response
+  Axios P10: Error Handling 1
+  - url not correct
+
+
+  (***) 3 cases: 
+    > Case 1: status code different than 200 range
+    > Case 2: request was made but no response from server
+    > Case 3: no request + no response
 
 */
 
-function transformResponse() {
-  const options = {
-    method: 'POST',
-    url: 'https://jsonplaceholder.typicode.com/todos',
+function errorHandling() {
+  axios
+    .get('https://jsonplaceholder.typicode.com/todoss', {})
+    .then((res) => showOutput(res))
+    .catch((err) => {
+      if (err.response) {
+        // Server responded with a status other than 200 range
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers)
 
-    // (1)
-    data: {
-      title: 'Hello World',
-    },
-
-    // (2) transform data to uppercase b4 sending it to server
-    transformResponse: axios.defaults.transformResponse.concat((data) => {
-      data.title = data.title.toUpperCase()
-      return data
-    }),
-  }
-
-  // (***)
-  axios(options).then((res) => showOutput(res))
+        if (err.response.status === 404) {
+          alert('Error: Page Not Found')
+        }
+      } else if (err.request) {
+        // Request was made but no response
+        console.error('> err.request', err.request)
+      } else {
+        // Error
+        console.error('> err.message', err.message)
+      }
+    })
 }
 
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse)
+document.getElementById('error').addEventListener('click', errorHandling)
