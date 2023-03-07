fae13fb18fb92981be200423a2239b4bec65fac7
Hung Bui <hungbui7690@gmail.com>
2023-03-06 17:50:02 -0700

    Axios P2: GET Method

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 /*
-  Axios P1: Initial Setup
-  - showOutput.js > import in .html file
+  Axios P2: GET Method
   
 */
+
+// GET REQUEST
+function getTodos() {
+  axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    params: {
+      _limit: 5, // param
+    },
+  })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err))
+}
+
+document.getElementById('get').addEventListener('click', getTodos)
