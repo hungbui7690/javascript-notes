ca8a4c98de0790226bd6d4cfc717489812865986
Hung Bui <hungbui7690@gmail.com>
2023-03-07 02:49:00 -0700

    Intersection Observer P8: Infinite Scrolling Example P1

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /*
-  Intersection Observer P6: rootMargin
-  > rootMargin: '-100px'
+  Intersection Observer P8: Infinite Scrolling Example P1
+  > root option
+    - right now, it is the .card-container > and observer tracks all of its children 
+      > we can set it to anything we want 
 
-
-  (***) rootMargin: '100px': 100px away > out of sight > we need to use inspector to check
 */
 
+const cardContainer = document.querySelector('.card-container')
 const cards = document.querySelectorAll('.card')
 
 const observer = new IntersectionObserver(
@@ -14,9 +15,34 @@ const observer = new IntersectionObserver(
       entry.target.classList.toggle('show', entry.isIntersecting)
     })
   },
-  { rootMargin: '100px' } // (***) we say that the container now is 100px smaller than normal container
+  { threshold: 1 }
 )
 
 cards.forEach((card) => {
   observer.observe(card)
 })
+
+// (1)
+const lastCardObserver = new IntersectionObserver((entries) => {
+  const lastCard = entries[0]
+
+  // (3)
+  if (!lastCard.isIntersecting) return
+
+  // (4)
+  loadNewCards()
+})
+
+// (2)
+lastCardObserver.observe(document.querySelector('.card:last-child'))
+
+// (5)
+function loadNewCards() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement('div')
+    card.textContent = 'New Card'
+    card.classList.add('card')
+    observer.observe(card)
+    cardContainer.append(card)
+  }
+}
