bec1a521f7e860545b9e76954533a0862ce7e53a
Hung Bui <hungbui7690@gmail.com>
2023-03-07 02:05:11 -0700

    Debounce P5

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 /*
-  Debounce P4:
-  - at (1), we invoke the function right away 
-  - but we don't want to invoke > (2) === return a function 
+  Debounce P5
+  - now it works
 
 */
 
 const btn = document.querySelector('.btn')
 
-// (2)
 const debounce = () => {
+  // (1)
+  let timeoutID
+
   return () => {
-    const timeoutID = setTimeout(() => {
+    console.log(timeoutID)
+    clearTimeout(timeoutID) // (2) this needs to be before setTimeout > clear out the last one
+
+    // (3)
+    timeoutID = setTimeout(() => {
       console.log('clicked ✈')
     }, 2000)
-
-    console.log(timeoutID)
-    clearTimeout(timeoutID)
   }
 }
 
-// (1)
 btn.addEventListener('click', debounce())
