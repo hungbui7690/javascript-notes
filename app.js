2e2dd81a71a3b92217baf92c66d7fae2b8bbf600
Hung Bui <hungbui7690@gmail.com>
2023-03-09 05:59:09 -0700

    Object.assign() P4: Merging objects with same properties

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*
-  Object.assign() P4: Merging objects
+  Object.assign() P4: Merging objects with same properties
 
 */
 
-const o1 = { a: 1 }
-const o2 = { b: 2 }
+const o1 = { a: 1, b: 1, c: 1 }
+const o2 = { b: 2, c: 2 }
 const o3 = { c: 3 }
 
-const obj = Object.assign(o1, o2, o3)
+const obj = Object.assign({}, o1, o2, o3) // (***) use {}
 console.log(obj) // { a: 1, b: 2, c: 3 }
-console.log(o1) // { a: 1, b: 2, c: 3 }, target object itself is changed.
+console.log(o1) // {a: 1, b: 1, c: 1}
