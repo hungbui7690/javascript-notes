48d4a76a8bde8c0b4d5e1508c6be4db6310f71d9
Hung Bui <hungbui7690@gmail.com>
2023-03-09 05:58:24 -0700

    Object.assign() P4: Merging objects

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /*
-  Object.assign() P3: Deep Clone P2: Solution
-  - For deep cloning, we need to use alternatives, because Object.assign() copies property values.
+  Object.assign() P4: Merging objects
 
 */
 
-// Deep Clone
-const obj3 = { a: 0, b: { c: 0 } }
-const obj4 = JSON.parse(JSON.stringify(obj3)) // (***) parse to String
+const o1 = { a: 1 }
+const o2 = { b: 2 }
+const o3 = { c: 3 }
 
-obj3.a = 4
-obj3.b.c = 4
-console.log(obj4) // { a: 0, b: { c: 0 } }
+const obj = Object.assign(o1, o2, o3)
+console.log(obj) // { a: 1, b: 2, c: 3 }
+console.log(o1) // { a: 1, b: 2, c: 3 }, target object itself is changed.
