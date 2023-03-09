fc60b27fffc2280eebc7321b1b88a5968be9f553
Hung Bui <hungbui7690@gmail.com>
2023-03-09 05:57:31 -0700

    Object.assign() P3: Deep Clone P2: Solution

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 /*
-  Object.assign() P3: Deep Clone P1: Problem
-  - because Object.assign() copies property values.
-  - If the source value is a reference to an object, it only copies the reference value.
+  Object.assign() P3: Deep Clone P2: Solution
+  - For deep cloning, we need to use alternatives, because Object.assign() copies property values.
 
 */
 
-const obj1 = { a: 0, b: { c: 0 } }
-const obj2 = Object.assign({}, obj1)
-console.log(obj2) // { a: 0, b: { c: 0 } }
+// Deep Clone
+const obj3 = { a: 0, b: { c: 0 } }
+const obj4 = JSON.parse(JSON.stringify(obj3)) // (***) parse to String
 
-obj1.a = 1
-console.log(obj1) // { a: 1, b: { c: 0 } }
-console.log(obj2) // { a: 0, b: { c: 0 } }
-
-obj2.a = 2
-console.log(obj1) // { a: 1, b: { c: 0 } }
-console.log(obj2) // { a: 2, b: { c: 0 } }
-
-obj2.b.c = 3 // (***) modify both
-console.log(obj1) // { a: 1, b: { c: 3 } }
-console.log(obj2) // { a: 2, b: { c: 3 } }
+obj3.a = 4
+obj3.b.c = 4
+console.log(obj4) // { a: 0, b: { c: 0 } }
