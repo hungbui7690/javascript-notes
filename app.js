247f68dfd15495f94b0490ef4c43aea9f1f57763
Hung Bui <hungbui7690@gmail.com>
2023-03-09 05:53:17 -0700

    Object.assign() P1

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 /*
+  Object.assign() P1
+  - use to copy object
+  - Object.assign(target, source)
+  - use in pre ES6
+  - after ES6, we can use spread operator
 
 */
 
-console.log('Hello World')
+const target = { a: 1, b: 2 }
+const source = { b: 4, c: 5 }
+
+const returnedTarget = Object.assign(target, source)
+
+console.log(returnedTarget) // {a: 1, b: 4, c: 5}
+console.log(target) // {a: 1, b: 4, c: 5}
+
+console.log(returnedTarget === target) // true
